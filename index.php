f9744edeb0c4f6d471b146fca17016b8f7f03a2a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-06-01 09:07:34 +0530

    Enhance navbar toggler styling for improved visibility on dark backgrounds

--- index.php
+++ index.php
@@ -47,8 +47,8 @@ $feedbacks = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3">
     <div class="container">
       <a class="navbar-brand" href="#">Food Plaza <i class="fa-solid fa-utensils ms-1"></i></a>     
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon text-white"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
